292d95cf0c9dadd8dbee68515d20e680bb903057
openvisual <67694546+openvisual@users.noreply.github.com>
2020-12-16 16:49:55 +0900

    **

--- image_stich.py
+++ image_stich.py
@@ -4,19 +4,18 @@ import cv2, numpy as np, matplotlib.pyplot as plt
 import glob, os
 
 # # Get images path
-IMG_NAME = 'boat'
+IMG_NAME = 'desk'
 
 img_list = []
 for ext in ('0*.gif', '0*.png', '0*.jpg'):
     img_list.extend(glob.glob(os.path.join('imgs', IMG_NAME, ext)))
+pass
 
 img_list = sorted(img_list)
 
 print(img_list)
 
-
 # # Load images
-
 imgs = []
 
 plt.figure(figsize=(20, 20))
@@ -27,17 +26,14 @@ for i, img_path in enumerate(img_list):
     
     plt.subplot(len(img_list) // 3 + 1, 3, i+1)
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
+pass
+plt.show()
 
 # # Stitch images
-
 mode = cv2.STITCHER_PANORAMA
 # mode = cv2.STITCHER_SCANS
 
-if int(cv2.__version__[0]) == 3:
-    stitcher = cv2.createStitcher(mode)
-else:
-    stitcher = cv2.Stitcher_create(mode)
+stitcher = cv2.Stitcher_create(mode)
     
 status, stitched = stitcher.stitch(imgs)
 
@@ -48,7 +44,11 @@ if status == 0:
     plt.imshow(cv2.cvtColor(stitched, cv2.COLOR_BGR2RGB))
 else:
     print('failed... %s' % status)
+    import sys
+    sys.exit( 0 )
+pass
 
+plt.show()
 
 # # Get image mask
 # - white 255
@@ -69,9 +69,12 @@ thresh_copy = thresh.copy()
 while np.sum(thresh_copy) > 0:
     thresh_copy = thresh_copy[1:-1, 1:-1]
     stitched_copy = stitched_copy[1:-1, 1:-1]
+pass
     
 cv2.imwrite(os.path.join('imgs', IMG_NAME, 'result_crop.jpg'), stitched_copy)
 
 plt.figure(figsize=(20, 20))
 plt.imshow(cv2.cvtColor(stitched_copy, cv2.COLOR_BGR2RGB))
+
+plt.show()
 
